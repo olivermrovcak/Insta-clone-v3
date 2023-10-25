4a6ff2bfda4ceac04d448c1233abcfe5d0e4e6b9
Oliver Mrovčák <oliver.mrovcak@prosoft.sk>
2023-10-25 22:07:22 +0200

    fix

---
+++ src/firebase/apiCalls.ts
@@ -0,0 +1,56 @@
+import {collection, doc, getDoc, getDocs} from "firebase/firestore";
+
+import {db} from '../firebase/firebase';
+
+export async function getDataFromFirebase(path: string) {
+    const postsCol = collection(db, path);
+    const postsSnapshot = await getDocs(postsCol);
+    return postsSnapshot.docs.map(doc => doc.data());
+}
+
+export async function getFollowersIds(userId: string) {
+    const postsCol = collection(db, 'users/' + userId + '/followers' );
+    const postsSnapshot = await getDocs(postsCol);
+    const retVal = postsSnapshot.docs.map(doc => doc.data().uid);
+    return retVal;
+}
+
+export async function getFollowersProfiles(userId: string) {
+    const followersCol = collection(db, 'users/' + userId + '/followers');
+    const followersSnapshot = await getDocs(followersCol);
+    const profilePromises = followersSnapshot.docs.map(async doc => {
+        const profile = await getProfile(doc.data().uid);
+        return profile;
+    });
+    const profiles = await Promise.all(profilePromises);
+    return profiles;
+}
+
+export async function getProfile(userId: string) {
+    const userDocRef = doc(db, 'users', userId);
+    const userDocSnapshot = await getDoc(userDocRef);
+    if (userDocSnapshot.exists()) {
+        return userDocSnapshot.data();
+    } else {
+        return null; // User document does not exist
+    }
+}
+
+export async function getFollowersCount(userId: string) {
+    const followersCol = collection(db, 'users/' + userId + '/followers');
+    const followersSnapshot = await getDocs(followersCol);
+    const followersCount = followersSnapshot.docs.length;
+    return followersCount;
+}
+
+export async function getFollowingCount(userId: string) {
+    const followingCol = collection(db, 'users/' + userId + '/following');
+    const followingSnapshot = await getDocs(followingCol);
+    const followingCount = followingSnapshot.docs.length;
+    return followingCount;
+}
+
+export async function getPosts() {
+    const path = 'posts';
+    return getDataFromFirebase(path);
+}
---
+++ src/firebase/functions.ts
@@ -0,0 +1,56 @@
+import {collection, doc, getDoc, getDocs, orderBy, query} from "firebase/firestore";
+import {db} from "./firebase";
+import {firestore} from "firebase-admin";
+
+
+interface Props {
+    path: string,
+    pathSegments?: string,
+    orderByField?: string
+    order?: string | any
+}
+
+async function getDataFromDb({path, pathSegments, orderByField, order}: Props): Promise<[]>;
+async function getDataFromDb({path, pathSegments}: Props): Promise<[]>;
+
+
+// posts: {
+//      id : {
+//          comments: {
+//                [obj],
+//                [obj]
+//          }
+//      }
+//
+// }
+
+//path example : "posts"
+//path segments example : "id/comments"
+
+async function getDataFromDb({path, pathSegments, orderByField, order}: Props) {
+    const q = query(collection(
+            db,
+            path,
+            pathSegments ? (pathSegments) : ("")),
+        orderBy(orderByField ? (orderByField) : ("timeStamp"), order ? (order) : ("desc"))
+    );
+    const querySnapshot = await getDocs(q);
+    var data: any = [];
+    querySnapshot.forEach((doc) => {
+        data = [...data, doc];
+    });
+
+    return data;
+
+};
+
+export async function getDocument({path, pathSegments}: Props) {
+    const ref = doc(db, path, pathSegments ? (pathSegments) : (""));
+    const docSnap = await getDoc(ref);
+    if (docSnap.exists()) {
+        return docSnap;
+    }
+}
+
+
+export default getDataFromDb;
